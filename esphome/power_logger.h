00172cde0ee6f03c3a9b7d2bf100f59c93d949f4
wesleygas <wesleygl16@gmail.com>
2021-03-31 21:34:52 -0300

    Plogger update

--- esphome/power_logger.h
+++ esphome/power_logger.h
@@ -1,7 +1,21 @@
 #include "esphome.h"
+#include "SD.h"
 
-class powerLogger : public Component, public Sensor{
+#define LOGFILENAME "plogger.csv"
+
+class powerLogger : public Component, public Sensor {
 public:
+    /**
+     * The constructor of the powerLogger class receives references for the current
+     * and voltage sensors created by the INA219 integration and uses their internal
+     * values to efficiently do the calculations
+     *
+     * @param int_dt    Internal deltaT, The minimum timestep for pooling the measurements
+     * @param inaCurr   Current sensor reference, received through the id(SENSOR_ID)
+     * @param inaColt   Voltage sensor reference, received through the id(SENSOR_ID)
+     * @param ina       Actual Component reference, received through the id(COMPONENT_ID)
+     * 
+     */
     powerLogger(uint16_t int_dt, Sensor *inaCurr, Sensor *inaVolt, PollingComponent *ina) : 
         int_dt(int_dt), inaCurrent(inaCurr), inaVoltage(inaVolt), inaComp(ina) {}
     
@@ -9,6 +23,7 @@ public:
     
     Sensor *capacity = new Sensor();
     Sensor *energy = new Sensor();
+    Sensor *sdLog = new Sensor();
 
     uint16_t int_dt;
 
@@ -16,25 +31,38 @@ public:
     Sensor *inaVoltage = nullptr;
     PollingComponent *inaComp = nullptr;
 
+    const int chipSelect = D8;  // used for ESP8266
+    bool sdCardPresent = true;
+
     unsigned long last_measurement = 0;
-    float net_mah = 0.0f;
-    float input_mah = 0.0f;
-    float output_mah = 0.0f;
-    
+    float net_mah = 0.0f;    
     float net_wh = 0.0f;
-    float input_wh = 0.0f;
-    float output_wh = 0.0f;
 
     float last_c = 0.0f;
     float last_v = 0.0f;
 
     void setup() override {
-        // This will be called by App.setup()
-        ESP_LOGW("custom", "This is from plog");
+        ESP_LOGD("custom", "Sending hello from plogger");
         inaComp->update();
         last_c = getCurr();
         last_v = getVolt();
+
+        if(!SD.begin(chipSelect)){
+            sdCardPresent = false;
+            ESP_LOGE("custom","SDCARD Initialization failed!");
+        } else if (!SD.exists(LOGFILENAME)){
+            File dataFile = SD.open(LOGFILENAME, FILE_WRITE);
+            if(dataFile){ 
+                //Create the CSV header 
+                dataFile.println("Voltage,Current,Power,mAh,mWh,Dt");
+                dataFile.close();
+            }
+        }
+
+        // Publish the SDCard presence so we can show it on the display
+        sdLog->publish_state((int)sdCardPresent);
     }
+
     void loop() override {
         if(millis() > last_measurement + int_dt){
             inaComp->update();
@@ -43,20 +71,14 @@ public:
             float new_c = getCurr();
             float new_v = getVolt();
             
+            //Integrate through the midpoint method
             float dmah = (((new_c - last_c)/2.0f)+last_c);
-            if(dmah != 0.0f){
-                dmah /= (3600.0f/(float)dt);            
+            if(dmah != 0.0f){ //Spare the calculations if value is zero
+                dmah *= ((float)dt/3600.0f); //convert our current uAs to mAh           
                 float dwh = (((new_v - last_v)/2.0f)+last_v)*dmah;
                 
                 net_mah += dmah;
                 net_wh += dwh;
-                if(dmah > 0.0f){
-                    input_mah+=dmah;
-                    input_wh+=dwh;
-                }else if(dmah < 0.0f){
-                    output_mah+=dmah;
-                    output_wh+=dwh;
-                }
             }
             
             ESP_LOGVV("custom", "mAh: %.5f || mWh: %.5f || Dt: %lu", net_mah, net_wh, dt);
@@ -65,9 +87,24 @@ public:
 
             last_c = new_c;
             last_v = new_v;
+
+            if(sdCardPresent){
+                File dataFile = SD.open("plogger.txt", FILE_WRITE);
+                if(dataFile){     
+                    // Write a new csv line to the sdcard
+                    //             Voltage(V),Current(A),Power(W),mAh,mWh,Dt
+                    dataFile.printf("%.3f,%.5f,%.4f,%.3f,%.3f,%lu\n",new_v, new_c, new_v*new_c, net_mah, net_wh, dt);
+                    dataFile.close();
+                }else{
+                    //if the file opening failed, disable sdcard altogether
+                    sdCardPresent = false;
+                    sdLog->publish_state((int)sdCardPresent);
+                }
+            }
         }
     }
 
+    //Helper functions to get the raw sensor data and do some filtering beforehand
     float getCurr(){
         float value = inaCurrent->raw_state;
         if(fabs(value) < 0.0002) value = 0.0;
@@ -79,15 +116,12 @@ public:
         return value;
     }
 
+    // In case we would need to reset the values without reseting the whole node
     void resetCapacity(){
         net_mah = 0.0f;
-        input_mah = 0.0f;
-        output_mah = 0.0f;
     }
 
     void resetEnergy(){
         net_wh = 0.0f;
-        input_wh = 0.0f;
-        output_wh = 0.0f;
     }
 };
